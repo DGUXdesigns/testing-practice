e166acee3a39f78651ddb1e47ab53d2506e74599
Daveian <140139328+DGUXdesigns@users.noreply.github.com>
2025-01-12 22:48:24 -0500

    Initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,23 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import eslintPluginPrettier from 'eslint-plugin-prettier/recommended';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    languageOptions: {
+      globals: { ...globals.node, ...globals.browser },
+    },
+  },
+  pluginJs.configs.recommended,
+  eslintPluginPrettier,
+  {
+    rules: {
+      'prettier/prettier': 'off', // Treat Prettier formatting issues as errors
+      'capitalized-comments': ['error', 'always'],
+      eqeqeq: ['error', 'always'], // Enforce strict equality (===)
+      'no-console': 'warn', // Warn when console.log is used
+      'no-unused-vars': 'warn',
+    },
+  },
+];
