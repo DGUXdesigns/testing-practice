191e1eb69fb55adceff5accd7c211925786374fc
Daveian <140139328+DGUXdesigns@users.noreply.github.com>
2025-01-13 00:05:18 -0500

    feat: Add testing for (calculator, capitalize, reverseString)

---
+++ src/practice.test.js
@@ -0,0 +1,36 @@
+import { describe, test, expect } from '@jest/globals';
+import { capitalize, reverseString, calculator } from './practice.js';
+
+test('Fist letter should be capitalized', () => {
+  expect(capitalize('champion')).toBe('Champion');
+});
+
+test('String should be reversed', () => {
+  expect(reverseString('tailor')).toBe('roliat');
+});
+
+describe('Calculator', () => {
+  test('adds two numbers', () => {
+    expect(calculator.add(2, 2)).toBe(4);
+    expect(calculator.add(5, 10)).toBe(15);
+    expect(calculator.add(8, 12)).toBe(20);
+  });
+
+  test('subtracts two numbers', () => {
+    expect(calculator.subtract(4, 2)).toBe(2);
+    expect(calculator.subtract(15, 10)).toBe(5);
+    expect(calculator.subtract(16, 5)).toBe(11);
+  });
+
+  test('divides two numbers', () => {
+    expect(calculator.divide(4, 2)).toBe(2);
+    expect(calculator.divide(30, 5)).toBe(6);
+    expect(calculator.divide(100, 4)).toBe(25);
+  });
+
+  test('Multiplies two numbers', () => {
+    expect(calculator.multiply(4, 2)).toBe(8);
+    expect(calculator.multiply(25, 5)).toBe(125);
+    expect(calculator.multiply(75, 4)).toBe(300);
+  });
+});
